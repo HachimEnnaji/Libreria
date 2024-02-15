f1084363b57269e6ec7d8819d52c277dd7c45e57
HachimEnnaji <ashennaji@hotmail.it>
2024-02-15 13:02:31 +0100

    Aggiungere i file di progetto.

---
+++ Libreria/Libreria/Default.aspx
@@ -0,0 +1,9 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="Libreria._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+  <main>
+      <asp:GridView ID="GridView1" runat="server" OnRowDataBound="GridView1_RowDataBound"></asp:GridView>
+    </main>
+
+</asp:Content>
---
+++ Libreria/Libreria/Default.aspx.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Libreria
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["myConnection"].ConnectionString.ToString();
+            SqlConnection conn = new SqlConnection(connectionString);
+
+
+            try
+            {
+                conn.Open();
+                SqlDataAdapter adapter = new SqlDataAdapter("SELECT * FROM Libreriaa", conn);
+                DataTable dataTable = new DataTable();
+                adapter.Fill(dataTable);
+
+                GridView1.DataSource = dataTable;
+
+                GridView1.DataBind();
+
+            }
+            catch (Exception ex)
+            {
+                Response.Write(ex.Message);
+            }
+            finally { conn.Close(); }
+
+        }
+
+        protected void GridView1_RowDataBound(object sender, System.Web.UI.WebControls.GridViewRowEventArgs e)
+        {
+            if (e.Row.RowType == DataControlRowType.DataRow)
+            {
+                int index = 4;
+                string imagePath = e.Row.Cells[index].Text;
+                Image img = new Image();
+                img.ImageUrl = imagePath;
+
+                img.Height = 150;
+
+                e.Row.Cells[index].Controls.Add(img);
+
+            }
+        }
+    }
+}
